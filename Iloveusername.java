fd5fe43f9ce84255cfb9b58d81671e3d914a4a2c
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2025-09-08 23:25:17 +0530

    contest

---
+++ Iloveusername.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+public class Iloveusername {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i<n ; i++){
+            a[i] = sc.nextInt();
+        }
+        int ct = 0; 
+        int mx = Integer.MIN_VALUE;
+        int mi = Integer.MAX_VALUE;
+        for(int i = 0 ; i< n ; i++){
+            if(){
+                ct++;
+            }
+        }
+        System.out.println(ct);
+    }
+}
